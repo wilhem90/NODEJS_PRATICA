9925830ccd7839d00ec7a0bdc06745e5aa26b4f2
Wilhem Wundt Maxime <wilhemmaxime97@gmail.com>
2024-01-19 14:26:28 -0300

    refresh

--- server.js
+++ server.js
@@ -3,11 +3,34 @@ const app = express()
 
 const porta = 8080
 
-app.get('/message', (req, res) => {
-    res.statusCode = 200
-    res.send("Hello World!")
-})
+function ServidorSimple(message) {
+    // rota message
+    app.get('/message', (req, res) => {
+        res.statusCode = 200
+        res.send(message)
+    })
 
-app.listen(porta, () => {
-    console.log(`Meu servidor esta rodando! http://localhost:${porta}`);
-})
+    // rota para receber um arquivo JSON
+    app.get('/jsonArquivos', (req, res) => {
+        res.statusCode(200)
+        res.send(
+            {
+                id: 1,
+                balance: 1805,
+                tipoMoeda: "BRL",
+                nome: "Wilhem Wundt",
+                sobrenome: "Maxime",
+                idade: 34,
+                pais: "Brasil",
+                estadoCivil: "Solteiro",
+                escolaridade: "Superior Imcompleto",
+            }
+        )
+    })
+    
+    app.listen(porta, () => {
+        console.log(`Meu servidor esta rodando! http://localhost:${porta}/message`);
+    })
+}
+
+ServidorSimple("Hello World! <br><br>Aqui esta meu primeiro servidor rodando!")
